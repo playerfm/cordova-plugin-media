29d22a6e23c0bc5ed12d4eb5b75b040f0f5a2dda
Nino Paolo Amarillento <o70vd@yahoo.com>
2014-03-19 19:32:04 +0800

    [Fixed] Removed cordova.define header

--- src/firefoxos/MediaProxy.js
+++ src/firefoxos/MediaProxy.js
@@ -1,4 +1,4 @@
-cordova.define("org.apache.cordova.media.FxosMedia", function(require, exports, module) { /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -257,4 +257,3 @@ module.exports = {
 };
 
 require('cordova/firefoxos/commandProxy').add('Media', module.exports);
-});
